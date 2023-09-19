88d3627f6364eb6413632bd9e4d91f8925e0a779
Sheshmani Patel <sumitkumar47581@gmail.com>
2023-09-19 09:51:22 +0530

    destructing the objects , fething the elements from the html file and populating the option div  with buttons

--- quiz.js
+++ quiz.js
@@ -1,9 +1,29 @@
-const questionObj = 
-    {
-      category: 'Food & Drink',
-      id: 'qa-1',
-      correctAnswer: 'Three',
-      answers: ['Two', 'Three ', 'Four', 'Five'],
-      question:
-        "How many pieces of bun are in a Mcdonald's Big Mac?",
-    };
+const questionObj = {
+  category: "Food & Drink",
+  id: "qa-1",
+  correctAnswer: "1999",
+  options: ["1995", "1999 ", "1996", "2000"],
+  question: "When was G20 established?",
+};
+
+const { question, correctAnswer, options } = questionObj;
+
+//Fetching elements 
+const questionEl = document.getElementById("question");
+const optionEl=document.getElementById("options");
+const scoreEl=document.getElementById("score");
+
+
+questionEl.textContent = question;
+
+
+// populating the option div with the buttons
+
+options.forEach((opt)=>{
+  const btn=document.createElement("button");
+  btn.textContent=opt;
+  optionEl.appendChild(btn);
+  
+})
+
+ 
